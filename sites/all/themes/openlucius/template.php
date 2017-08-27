917180ff8f73de62174ca54505b3560c2b0a66b7
Muhammad Andriansyah Nur Cahya <andriansyahnc@users.noreply.github.com>
2017-08-28 06:45:35 +0800

    Finishing

--- sites/all/themes/openlucius/template.php
+++ sites/all/themes/openlucius/template.php
@@ -528,7 +528,7 @@ function openlucius_preprocess_views_view(&$variables) {
       }
     }
   }
-
+  
   // Page 'all-users'.
   if ($view->name == 'users_in_groups' && $view->current_display == 'page_1') {
     if (user_access("administer users")) {
@@ -1753,7 +1753,7 @@ function openlucius_preprocess_views_view_field(&$vars) {
     $replacement = array(
       '@epic_all' => $count_all,
       '@epic_start' => $stop_percent,
-      '@epic_stop' => $stop_percent,
+      '@epic_stop' => $start_percent,
       '@in_progress' => $inp_percent,
       '@id' => 'epic-progress-' . $vars['row']->nid,
       '@text' => $text
@@ -1774,4 +1774,11 @@ function openlucius_preprocess_views_view_field(&$vars) {
 
     $vars['output'] = t($output, $replacement);
   }
+
+  if($vars['theme_hook_original'] == 'views_view_field__milestone_list__page__title') {
+    $milestone_title = $vars['row']->node_field_data_field_milestone_title;
+    
+    
+  }
+  
 }
--- sites/all/themes/openlucius/theme/views/openlucius_milestone/views-view-unformatted--milestone-list--page.tpl.php
+++ sites/all/themes/openlucius/theme/views/openlucius_milestone/views-view-unformatted--milestone-list--page.tpl.php
@@ -6,24 +6,89 @@
  *
  * @ingroup views_templates
  */
+
+if($title != NULL) {
+  $epic_start = variable_get('openlucius_epics_progress_epic_start_todo_state', '');
+  $epic_stop = variable_get('openlucius_epics_progress_epic_close_todo_state', '');
+
+  $epic_start_state = taxonomy_term_load(variable_get('epic_start_state', ''))->name;
+  $epic_inp_state = taxonomy_term_load(variable_get('epic_in_progress_state', ''))->name;
+  $epic_stop_state = taxonomy_term_load(variable_get('epic_close_state', ''))->name;
+  $views_count_all = views_get_view_result('vw_milestone_get_story_property', 'master', $title);
+  $count_all = !empty($views_count_all) ? $views_count_all[0]->field_todo_list_reference_node_nid : 0;
+
+  $views_count_start = views_get_view_result('vw_milestone_get_story_property', 'master', $title, $epic_start);
+  $count_start = !empty($views_count_start) ? $views_count_start[0]->field_todo_list_reference_node_nid : 0;
+  
+  $views_count_complete = views_get_view_result('vw_milestone_get_story_property', 'master', $title, $epic_stop);
+  $count_stop = !empty($views_count_complete) ? $views_count_complete[0]->field_todo_list_reference_node_nid : 0;
+
+  $in_progress =  $count_all - ($count_start + $count_stop);
+
+  $start_percent = ($count_start / $count_all * 100);
+  $inp_percent = ($in_progress / $count_all * 100);
+  $stop_percent = ($count_stop / $count_all * 100);
+  
+  $state = t('In Progress');
+
+  if($start_percent == 100) {
+    $state = t('To Do');
+  } else if ($stop_percent == 100) {
+    $state = t('Completed');
+  }
+
+  if($count_all == 0) {
+    $texts[] = '0% ' . $epic_stop_state;
+  } else {
+    $texts[] = round($stop_percent, 2) . '% ' . $epic_stop_state;
+  }
+  
+  $replacement = array(
+    '@epic_all' => $count_all,
+    '@epic_start' => $stop_percent,
+    '@epic_stop' => $start_percent,
+    '@in_progress' => $inp_percent,
+    '@id' => 'epic-progress-' . $title,
+    '@text' => implode(', ', $texts)
+  );
+
+  $output = node_load($title)->title;
+  $vars['output'] = '<div class="milestone-card-title">' . $output . '</div>';
+  $output_extra = '<div class="milestone-card-header-body">' .
+    "<div class='epic-progress progress-text'>@text</div>" .
+    '<div class="epic-progress progress" id="@id" ' . 
+    "data-attr-all='@epic_all'" . 
+    "data-attr-start='@epic_start'" . 
+    "data-attr-stop='@epic_stop'" . 
+    "data-attr-in-progress='@in_progress'>" . 
+      "<div class='progress-bar progress-epic-start' role='progressbar' style='width:@epic_start%'></div>" . 
+      "<div class='progress-bar progress-epic-in-progress' role='progressbar' style='width:@in_progress%'></div>" . 
+      "<div class='progress-bar progress-epic-stop' role='progressbar' style='width:@epic_stop%'></div>" . 
+    "</div></div>";
+  $output_extra = t($output_extra, $replacement);
+  $second_output = '<div class="milestone-state">' . 
+      '<div class="milestone-state-status"><i class="fa fa-paper-plane" aria-hidden="true"></i>' . $state . '</div>' .
+      '<div class="milestone-state-count"><i class="fa fa-flag" aria-hidden="true"></i>' . count($rows) . '</div>' .
+    '</div>';
+  $title = $output . $second_output;
+  $title = $title . $output_extra;
+}
 ?>
 <div class="milestone-card<?php print empty($title) ? '-empty': ''; ?>">
   <div class="milestone-card-header">
-    <div class="milestone-card-title">
-      <?php if (!empty($title)): ?>
+    <?php if (!empty($title)): ?>
+      <div class="milestone-card-title">
         <?php print $title; ?>
-      <?php else: ?>
+      </div>
+    <?php else: ?>
+      <div class="milestone-card-title">
         <?php print t('Epic Backlog') ?>
-      <?php endif; ?>
-    </div>
-
-    <div class="milestone-card-header-body">
-      <?php if (!empty($title)): ?>
-        <?php //print $title; ?>
-      <?php else: ?>
+      </div>
+      <div class="milestone-card-header-body">
         <?php print t('Once you have unfinished Epics that don\'t already belong to a Milestone, they will be listed here.'); ?>
-      <?php endif; ?>
-    </div>
+      </div>
+    <?php endif; ?>
+    
   </div>
 
   <div class="milestone-card-body">
